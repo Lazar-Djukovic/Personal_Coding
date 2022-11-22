4779fcefa015dba1e0dda9a7653dacd6567b1424
Lazar-Djukovic <ldjukovic40@gmail.com>
2022-11-22 11:53:49 +0000

    started course

---
+++ 1. Python/6. Automate the Boring Stuff with Python Programming/1. Basics/1.3 first program.py
@@ -0,0 +1,8 @@
+print('Hello world!')
+print('What is your name?')
+name = input()
+print('Nice to meet you '+ name)
+print('The lenght of your name is ' + str(len(name)))
+print('What is your age?')
+age = int(input('>'))
+print('You will be ' + str(age+1) + ' next year.')
